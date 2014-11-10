a4e18ffcb0e52e039ccdc32e64de19a12dee15ca
joaopedro <joaopedro@joaopedro-HP-Pavilion-dv6-Notebook-PC>
2014-11-10 17:56:47 +0000

    vamos ver o que isto dá

---
+++ WebContent/Javascript/utilities.js
@@ -0,0 +1,93 @@
+function initTour(){
+	var simpleGuide = {
+			  id: 'simple',
+			  'title': '',
+			  steps: [
+				{
+				    target: '#search_button',
+				    content: 'Search by artist, music name or values of Arousal and Valence',
+				    direction: 'bottom',
+				},
+				{
+				    target: '#feedback_button',
+				    content: 'Here you can see what is happening with your videos inside our system',
+				    direction: 'bottom'
+				},
+				
+				{
+				    target: '#importFile',
+				    content: 'Upload your formated .txt file with all the video URL you want',
+				    direction: 'top'
+				},
+				{
+				    target: '#importLink',
+				    content: 'Paste here your video URL',
+				    direction: 'top'
+				},
+				{
+				    target: '#library_button',
+				    content: 'Check the validated videos on the system',
+				    direction: 'top'
+				},
+				{
+				    target: '#now_playing_div',
+				    content: 'Video playing at the moment',
+				    direction: 'bottom'
+				},
+				{
+				    target: '#russel_div',
+				    content: 'Your video Arousal and Valence values will be displayed here',
+				    direction: 'bottom'
+				  },
+			    
+			    {
+			      target: '#lyrics_li',
+			      content: 'The video lyrics appear in this section',
+			      direction: 'bottom',
+			     
+			    },
+			    
+			    
+			  ]
+			};
+	
+	var optionsGuideOverrides = {
+			  autoAdvanceInterval: 10,      // Rotate through the steps at 10 second intervals
+			  step: {
+			    shadow: false
+			  }
+			}
+	
+		  $.pageguide(simpleGuide,optionsGuideOverrides);
+}
+
+function updateBarsPos(ver){
+		var extraHeight = 0;
+		var extraWidth = 0;
+		if(ver == 1){
+			if ($('#collapseLibrary').hasClass('in')){//($('#library_button').hasClass('collapsed'))
+				extraHeight= -230	;
+				extraWidth = 4;
+			}
+			else{
+				extraHeight= 230;
+				extraWidth = -4;
+			}
+		}
+		console.log("update "+extraHeight);
+		var scumtop = $('#progressBar').position().top-31+extraHeight;
+		var scumw = $('#progressBar').width()+extraWidth;
+		
+		$('#scumDiv').css({"background-color": '#FFFFFF', "width": scumw, "height": '27px', "position": 'absolute', "top": scumtop});
+		
+		$(".legacydiv").each(function( index ) {
+			  var emotionbartop = $('#progressBar').position().top-40+extraHeight;
+			  var emotionbarleft = $('#progressBar').position().left;
+			  var progbarW = getprogbarw();
+			  
+			  if(index == 0)
+				$(this).css({"position": 'absolute' , "width" : progbarW, "top": emotionbartop});
+			  else
+			  	$(this).css({"position": 'absolute' , "width" : progbarW, "top": emotionbartop , "left": index*progbarW+emotionbarleft});
+		});
+	}	
