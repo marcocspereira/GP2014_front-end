b66124d3e616181ef154af711b08704e853aab48
Marco Pereira <macsp@student.dei.uc.pt>
2014-12-09 16:26:11 +0000

    meu commit

--- src/servlet/SearchServlet.java
+++ src/servlet/SearchServlet.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -32,6 +33,7 @@ import edu.dei.gp.jpa.aux.DominantEmotion;
 public class SearchServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
+    @EJB
     FrontEndBeanRemote frontendBean;
 
     /**
@@ -79,16 +81,23 @@ public class SearchServlet extends HttpServlet {
 
 	// TODO retirar esta arraylist q serve de teste a pesquisa de musicas
 	List<Song> tempSongs = new ArrayList<Song>();
-	tempSongs.add(new Song(0, "Sia", "Chandelier", "-KXPLT2Xk5k", "vai levar na rata", 1, (float) 0.4,
+	tempSongs.add(new Song(0, "Sia", "Chandelier", "-KXPLT2Xk5k", "Lalalalala", 1, (float) 0.4,
 		DominantEmotion.Contentment, (float) 0.3));
 	tempSongs.get(tempSongs.size() - 1).setEmotions(emo);
-	tempSongs.add(new Song(1, "Katy Perry", "Roar", "9dgng_ekbV0", "berra que nem uma leoa", (float) -0.2, (float) 1.4,
+	tempSongs
+	.add(new Song(
+		1,
+		"Katy Perry",
+		"Roar",
+		"9dgng_ekbV0",
+		"Come up to meet you, Tell you I'm sorry, You don't know how lovely you are. I had to find you, Tell you I need you, Tell you I set you apart. Tell me your secrets, And ask me your questions, Oh let's go back to the start. Runnin' in circles, Comin' up tails, Heads on a science apart. Nobody said it was easy, It's such a shame for us to part. Nobody said it was easy, No one ever said it would be this hard. Oh take me back to the start. I was just guessin', At numbers and figures, Pullin' the puzzles apart. Questions of science, Science and progress, Do not speak as loud as my heart. Tell me you love me, Come back and haunt me, Oh, what a rush to the start. Runnin' in circles, Chasin' our tails, Comin' back as we are. Nobody said it was easy, Oh it's such a shame for us to part. Nobody said it was easy, No one ever said it would be so hard. I'm goin' back to the start. Oh ooh ooh ooh ooh ohh, Ah ooh ooh ooh ooh ooh, Oh ooh ooh ooh ooh ohh, Oh ooh ooh ooh ooh ohh.",
+		(float) -0.2, (float) 1.4,
 		DominantEmotion.Hapiness, (float) 0.4));
 	tempSongs.get(tempSongs.size() - 1).setEmotions(emo);
 	tempSongs.add(new Song(2, "Sam Smith", "Stay with", "uTTXJM5woJ8", "fica comigo que tenho frio", 1, (float) -0.4,
 		DominantEmotion.Melancholy, (float) 0.5));
 	tempSongs.get(tempSongs.size() - 1).setEmotions(emo);
-	tempSongs.add(new Song(3, "John Legend", "All Of Me", "Mk7-GRWq7wA", "tudo em mim\nentra em ti\nohohohoh",
+	tempSongs.add(new Song(3, "John Legend", "All Of Me", "Mk7-GRWq7wA", "tudo em mim\nBLABLABLA\nohohohoh",
 		(float) -0.4, (float) 0.4, DominantEmotion.Contentment, (float) 0.6));
 	tempSongs.get(tempSongs.size() - 1).setEmotions(emo);
 	tempSongs.add(new Song(4, "James Arthur", "Impossible", "1lefGrqcC1A", "impossiveeeeeeel", 1, (float) 0.9,
@@ -173,11 +182,11 @@ public class SearchServlet extends HttpServlet {
 
 		    ArrayList<SongStatus> singthesong = new ArrayList<SongStatus>();
 		    AVMoodTrack lolo = new AVMoodTrack(1, 1, 1, 1);
-		    Collection<AVMoodTrack> caralho = new ArrayList<AVMoodTrack>();
-		    caralho.add(lolo);
+		    Collection<AVMoodTrack> caral = new ArrayList<AVMoodTrack>();
+		    caral.add(lolo);
 		    Artist artist = new Artist("chief of them");
 		    edu.dei.gp.jpa.Song s = new edu.dei.gp.jpa.Song("does not push", "4b95MCszOxY", "xupa.txt",
-			    "pelos pubicos", 1, "c:\\cona.png", artist, caralho, 1, 1, DominantEmotion.Anxiety);
+			    "sim eu sei", 1, "c:\\ficheiro.png", artist, caral, 1, 1, DominantEmotion.Anxiety);
 		    SongStatus ss = new SongStatus(s);
 		    singthesong.add(ss);
 		    GenericSongPack<SongStatus> songStatus = new GenericSongPack<SongStatus>(singthesong, 1, 1);
@@ -203,7 +212,10 @@ public class SearchServlet extends HttpServlet {
 
 		    System.out.println("get all musics");
 
-		    //bean.getAllMusics();
+		    /* ja funciona
+		    GenericSongPack<SongLight> chefe = frontendBean.getReadyLighSongs(1);
+		    List<SongLight> chefedelas = chefe.getListContents();
+		     */
 		    // Temp
 		    String json = new Gson().toJson(tempSongs);
 		    out.write(json);
