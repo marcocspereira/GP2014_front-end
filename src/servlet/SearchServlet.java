fac9e2f535925975e60d472193d4da581e258f1a
joaopedro <joaopedro@joaopedro-HP-Pavilion-dv6-Notebook-PC>
2014-12-12 01:03:09 +0000

    paginaçao total (em principio) perfeita :)

--- src/servlet/SearchServlet.java
+++ src/servlet/SearchServlet.java
@@ -25,7 +25,7 @@ import edu.dei.gp.jpa.Song;
 @WebServlet("/SearchServlet")
 public class SearchServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-    private static final boolean debug = false;
+    private static final boolean debug = true;
 
     @EJB
     FrontEndBeanRemote frontendBean;
@@ -68,7 +68,8 @@ public class SearchServlet extends HttpServlet {
 		    }
 
 		    // communicates with back-end
-		    GenericSongPack<SongLight> textSongs = frontendBean.searchAuthorAndTitle(toSearch, page);
+		    GenericSongPack<SongLight> textSongs = frontendBean.getReadyLighSongs(page);//TESTE TESTE TESTE
+		    //GenericSongPack<SongLight> textSongs = frontendBean.searchAuthorAndTitle(toSearch, page);
 		    // processing back-end response in order to send to AJAX
 		    if (textSongs != null) {
 			String json = new Gson().toJson(textSongs);
@@ -96,8 +97,9 @@ public class SearchServlet extends HttpServlet {
 		    float maxValence = Float.parseFloat(request.getParameter("maxValence"));
 
 		    // Sends to frontendBean with values and page to search for
-		    GenericSongPack<SongLight> avSongs = frontendBean.searchArousalAndValenceValues(minArousal, maxArousal,
-			    minValence, maxValence, page);
+		    GenericSongPack<SongLight> avSongs = frontendBean.getReadyLighSongs(page);//TESTE TESTE TESTE
+//		    GenericSongPack<SongLight> avSongs = frontendBean.searchArousalAndValenceValues(minArousal, maxArousal,
+//			    minValence, maxValence, page);
 
 		    // processing back-end response in order to send to AJAX
 		    if (avSongs != null) {
